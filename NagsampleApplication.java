69b8f21af8063a3e93d7e17e4796ce797c20704d
Nagaraju Avaru <77373201+NA185117@users.noreply.github.com>
2021-01-27 23:29:33 +0530

    second change

---
+++ NagsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagsampleApplication.class, args);
+    }
+
+}
